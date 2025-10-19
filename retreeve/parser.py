1630cb6395aa2b9d9bcdee677990e63a8bf7e8d5
l1laxs <msferza@gmail.com>
2025-10-19 14:39:06 +0200

    update parser

--- retreeve/parser.py
+++ retreeve/parser.py
@@ -1,13 +1,12 @@
 from typing import TextIO
-# from .handlers.special import SpecialHandler
-from .tree import Tree
+from .handler_tree import HandlerTree
+from .parser_tree import ParseTree
 
 
 class Parser:
     def __init__(self, handlers: list):
-        self.handlers_tree = Tree(handlers)
-        # self.handler = SpecialHandler(handlers)
-        self.parsed_obj = Tree([])
+        self.handlers_tree = HandlerTree(handlers)
+        self.parsed_obj = ParseTree([])
 
     def parse(self, stream: TextIO):
         while line := stream.readline():
@@ -23,7 +22,7 @@ class Parser:
                     return
 
             # look for new handler in children
-            for child in self.handlers_tree.current_children():
+            for handler in self.handlers_tree.current_children():
                 handled = handler(line)
                 if handled:
                     self.parsed_obj.add_child(handled)
