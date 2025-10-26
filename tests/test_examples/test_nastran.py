731f928aee4dfd5ee7d4a4e9b09a9959f344d1d0
l1laxs <msferza@gmail.com>
2025-10-26 07:50:07 +0100

    Add test assertions

--- tests/test_examples/test_nastran.py
+++ tests/test_examples/test_nastran.py
@@ -20,4 +20,50 @@ def test_basic_example():
     example_module = load_module_from_path("run_example_module", example_path)
 
     # Call the function inside the loaded module
-    pprint(example_module.run_example())
+    result = example_module.run_example()
+
+    assert isinstance(result, list)
+    assert len(result) == 4
+    assert result[0].__class__.__name__ == "FallbackHandler"
+    assert result[1].__class__.__name__ == "SolHandler"
+    assert result[2].__class__.__name__ == "ExecControlHandler"
+    assert result[3].__class__.__name__ == "BulkHandler"
+
+    bulk = result[3]._contents
+    assert len(bulk) == 32
+    assert bulk[0].__class__.__name__ == "String"
+    assert bulk[1].__class__.__name__ == "CommentHandler"
+    assert bulk[2].__class__.__name__ == "CardHandler"
+    assert bulk[3].__class__.__name__ == "CardHandler"
+    assert bulk[4].__class__.__name__ == "CardHandler"
+    assert bulk[5].__class__.__name__ == "CardHandler"
+    assert bulk[6].__class__.__name__ == "CardHandler"
+    assert bulk[7].__class__.__name__ == "FallbackHandler"
+    assert bulk[8].__class__.__name__ == "CommentHandler"
+    assert bulk[9].__class__.__name__ == "CardHandler"
+    assert bulk[10].__class__.__name__ == "FallbackHandler"
+    assert bulk[11].__class__.__name__ == "CommentHandler"
+    assert bulk[12].__class__.__name__ == "CardHandler"  # PSHELL
+    assert len(bulk[12]._contents) == 2
+    assert bulk[13].__class__.__name__ == "FallbackHandler"
+    assert bulk[14].__class__.__name__ == "CommentHandler"
+    assert bulk[15].__class__.__name__ == "CardHandler"
+    assert bulk[16].__class__.__name__ == "CardHandler"
+    assert bulk[17].__class__.__name__ == "CardHandler"
+    assert bulk[18].__class__.__name__ == "CardHandler"
+    assert bulk[19].__class__.__name__ == "FallbackHandler"
+    assert bulk[20].__class__.__name__ == "CommentHandler"
+    assert bulk[21].__class__.__name__ == "CardHandler"  # MPC
+    assert len(bulk[21]._contents) == 2
+    assert bulk[22].__class__.__name__ == "FallbackHandler"
+    assert bulk[23].__class__.__name__ == "CommentHandler"
+    assert bulk[24].__class__.__name__ == "CardHandler"
+    assert bulk[25].__class__.__name__ == "FallbackHandler"
+    assert bulk[26].__class__.__name__ == "CommentHandler"
+    assert bulk[27].__class__.__name__ == "CardHandler"
+    assert bulk[28].__class__.__name__ == "CardHandler"
+    assert bulk[29].__class__.__name__ == "FallbackHandler"
+    assert bulk[30].__class__.__name__ == "CommentHandler"
+    assert bulk[31].__class__.__name__ == "String"
+
+    pprint(result)
